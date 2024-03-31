f9c2709ce9cd63639bcbb4f4978df83bf18d8d6b
Amiriel01 <quinney.sarah@gmail.com>
2024-03-31 09:33:52 -0500

    select made for allergen and meal categories

--- src/app/features/recipe-details/add-recipe-details/add-recipe-details.component.ts
+++ src/app/features/recipe-details/add-recipe-details/add-recipe-details.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddRecipeDetails } from '../models/add-recipe-details.model';
 import { RecipeDetailsService } from '../services/recipe-details.service';
 import { Router } from '@angular/router';
+import { MealCategoryService } from '../../meal-category/services/meal-category.service';
+import { AllergenCategoryService } from '../../allergen-category/services/allergen-category.service';
+import { Observable } from 'rxjs';
+import { MealCategory } from '../../meal-category/models/meal-category.model';
+import { AllergenCategory } from '../../allergen-category/models/allergen-category.model';
 
 @Component({
   selector: 'app-add-recipe-details',
   templateUrl: './add-recipe-details.component.html',
   styleUrls: ['./add-recipe-details.component.css']
 })
-export class AddRecipeDetailsComponent {
+export class AddRecipeDetailsComponent implements OnInit {
   model: AddRecipeDetails;
+  mealCategories$?: Observable<MealCategory[]>;
+  allergenCategories$?: Observable<AllergenCategory[]>;
 
-  constructor(private recipeDetailsService: RecipeDetailsService, private router: Router) {
+  constructor(private recipeDetailsService: RecipeDetailsService, private router: Router, private mealCategoryService: MealCategoryService, private allergenCategoryService: AllergenCategoryService) {
     this.model = {
       recipeName: '',
       recipeUrlHandle: '',
@@ -20,6 +27,11 @@ export class AddRecipeDetailsComponent {
       recipeImage: '',
       isVisible: true,
     }
+  }
+
+  ngOnInit(): void {
+    this.mealCategories$ = this.mealCategoryService.getAllMealCategories();
+    this.allergenCategories$ = this.allergenCategoryService.getAllergenCategories();
   }
 
   onFormSubmit(): void {
